720681015f74b543d0d191f60dc1e9e7c619fa7b
KMA Solaiman <ksolaima@purdue.edu>
2017-09-26 20:23:40 -0400

    calls the functions to load all sesor and plugin data into matlab variables and saves them

--- data_collection.m
+++ data_collection.m
@@ -5,6 +5,8 @@ gyro = importsensorfile('dataset_1/0_synced_10.186.6.43_2017-09-22_Sensor_gyr.tx
 lin = importsensorfile('dataset_1/0_synced_10.186.6.43_2017-09-22_Sensor_lin.txt',';');
 mag = importsensorfile('dataset_1/0_synced_10.186.6.43_2017-09-22_Sensor_mag.txt',';');
 
+% For example, acc.X is a (n*1) array and others also
+
 % get the plugin/label data from text file into variables
 plugin = importlabelfile('dataset_1/0_synced_128.10.130.37_Plugin_2017-09-22-label.txt', [1,78,178], [70,172,356]);
 
